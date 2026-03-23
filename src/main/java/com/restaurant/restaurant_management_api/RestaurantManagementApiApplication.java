308965a067aef043494b5495a7e3710da5205b8b
오혜성 <ohs020105@gmail.com>
2026-03-23 16:24:54 +0900

    chore: Spring Boot 초기 프로젝트 세팅

---
+++ src/main/java/com/restaurant/restaurant_management_api/RestaurantManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.restaurant_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantManagementApiApplication.class, args);
+	}
+
+}
